31296c714503366c3934728c420befee21e84fa5
rehma <rehma@COLLIERS>
2022-09-08 21:56:08 -0600

    Add project files.

---
+++ TodoApi/ListingDataRecord.cs
@@ -0,0 +1,11 @@
+namespace TodoApi
+{
+    public class ListingDataRecord
+    {
+        public string Id { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Description { get; set; }
+    }
+}
